b8d21ed5a1479ff95bf0ecabef9ec47c7ffcd54e
boonki <60532183+boonki@users.noreply.github.com>
2024-05-09 13:28:32 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
 			noExternal: ["astro", "@astrojs/image"],
 		},
 	},
-	site: 'https://vijiatjack.github.io',
-  	base: '/nookoffice'
+	site: 'https://boonki.github.io',
+  	base: '/ACNH_noautoplay'
 });
 
